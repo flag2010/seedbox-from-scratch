03f5ba85dcaab15e91be6edf1ad750be2bf21248
flag2010 <gqzhyzh@qq.com>
2013-06-17 22:06:13 +0800

    Update seedbox-from-scratch.sh

--- seedbox-from-scratch.sh
+++ seedbox-from-scratch.sh
@@ -22,7 +22,7 @@
 #
 ######################################################################
 #
-#  git clone -b master https://github.com/Notos/seedbox-from-scratch.git /etc/seedbox-from-scratch
+#  git clone -b master https://github.com/flag2010/seedbox-from-scratch.git /etc/seedbox-from-scratch
 #  sudo git stash; sudo git pull
 #
 #
@@ -270,7 +270,7 @@ apt-get --yes update
 apt-get --yes install whois sudo makepasswd git
 
 rm -f -r /etc/seedbox-from-scratch
-git clone -b v$SBFSCURRENTVERSION1 https://github.com/Notos/seedbox-from-scratch.git /etc/seedbox-from-scratch
+git clone -b v$SBFSCURRENTVERSION1 https://github.com/flag2010/seedbox-from-scratch.git /etc/seedbox-from-scratch
 mkdir -p cd /etc/seedbox-from-scratch/source
 mkdir -p cd /etc/seedbox-from-scratch/users
 
